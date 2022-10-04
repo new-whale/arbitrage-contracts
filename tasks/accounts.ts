17b3d34a7d4d46cd1c49ab652e568c308bcd9ddb
Jeongwoo Ji <jeongwooji@prestolabs.io>
2022-10-04 17:53:57 +0900

    Print balance of account

--- tasks/accounts.ts
+++ tasks/accounts.ts
@@ -1,10 +1,10 @@
 import { Signer } from '@ethersproject/abstract-signer';
 import { task } from 'hardhat/config';
 
-task('accounts', 'Prints the list of accounts', async (_taskArgs, hre) => {
-  const accounts: Array<Signer> = await hre.ethers.getSigners();
+task('accounts', 'Prints the list of accounts', async (_taskArgs, { ethers }) => {
+  const accounts: Array<Signer> = await ethers.getSigners();
 
   for (const account of accounts) {
-    console.log(await account.getAddress());
+    console.log(await account.getAddress(), ':', ethers.utils.formatEther(await account.getBalance()));
   }
 });
