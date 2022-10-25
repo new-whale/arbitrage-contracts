42e95f26ef03c981d4aa537c5e66b23b2423c953
Jeongwoo Ji <jeongwooji@prestolabs.io>
2022-10-25 18:45:51 +0900

    Fix I4IAdapter

--- contracts/SmartRoute/adapter/I4IAdapter.sol
+++ contracts/SmartRoute/adapter/I4IAdapter.sol
@@ -51,17 +51,21 @@ contract I4IAdapter is IRouterAdapter {
         address to
     ) external override returns (uint256 _output) {
         (address pool, uint256 tp, uint256 ncoin) = abi.decode(moreInfo, (address, uint256, uint256));
+        uint256 value;
+        if (fromToken == address(0)) {
+            value = amountIn;
+        }
 
         IERC20(fromToken).universalApproveMax(pool, amountIn);
         if (tp == 0) { // swap
             uint256 i = II4ISwapPool(pool).coinIndex(fromToken);
             uint256 j = II4ISwapPool(pool).coinIndex(toToken);
-            _output = II4ISwapPool(pool).exchange(i, j, amountIn, 1);
+            _output = II4ISwapPool(pool).exchange{ value: value }(i, j, amountIn, 1);
         } else if (tp == 1) { // token -> lp
             uint256 i = II4ISwapPool(pool).coinIndex(fromToken);
             uint256[] memory amounts = new uint256[](ncoin);
             amounts[i] = amountIn;
-            _output = II4ISwapPool(pool).addLiquidity(amounts, 1);
+            _output = II4ISwapPool(pool).addLiquidity{ value: value }(amounts, 1);
         } else if (tp == 2) { // lp -> token
             uint256 j = II4ISwapPool(pool).coinIndex(toToken);
             _output = II4ISwapPool(pool).removeLiquidityOneCoin(amountIn, j, 1);
